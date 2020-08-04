876e8350696fd8ef9f4be8924ae286c7c23f554e
Zeeson <olabrazanislam@gmail.com>
2020-08-04 09:08:05 -0700

    files modified

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 
 const fs = require('fs');
+const path = require("path");
 
 let notes = ["Node server", "Window", 'File System', 'Http']
 
-
+// create and write to file
 fs.writeFile('notes.txt', notes, (err) => {
       if (err) throw err;
     console.log('Note saved!');
 });
+
+// update a file
+fs.appendFile('notes.txt', 'github submission', notes, (err) => {
+    if (err) throw err;
+    console.log(' Note Updated!');
+  });
+
+//   rename a file
+fs.renameSync('notes.txt', './note.txt');
+
+// rename directory
+fs.renameSync('./NotesApp', './NoteApp');
+
+
+
+// const pathToDir = path.join(__dirname, "NotesApp")
+
+// fs.rmdir(pathToDir, (err) => {
+//   if (err) {
+//     throw err
+//   } else {
+//     console.log("Successfully removed the empty directory!")
+//   }
+// });
+
+// append to array of notes
+let stream = fs.createWriteStream("note.txt", {flags:'a'});
+[...Array(10000)].forEach((item, index) => {
+    stream.write(index + "\n");
+});
+stream.end();
